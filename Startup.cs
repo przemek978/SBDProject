74fc59cf115879b624cfac5447d55ed4c9712572
przemek978 <przemek.ku2000@gmail.com>
2022-11-14 20:41:09 +0100

    modified usesql

--- Startup.cs
+++ Startup.cs
@@ -28,7 +28,7 @@ namespace SBD
             services.AddRazorPages();
 
             services.AddDbContext<AirPortContext>(options =>
-                    options.UseOracle(Configuration.GetConnectionString("AirPortContext")));
+                    options.UseSqlServer(Configuration.GetConnectionString("AirPortContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
