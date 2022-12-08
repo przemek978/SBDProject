351441ff761ba0015704b18bb4101f761316d8ef
kamilkarwowski <karwowski596@gmail.com>
2022-12-08 17:47:51 +0100

    .

--- Models/Lot.cs
+++ Models/Lot.cs
@@ -9,20 +9,26 @@ namespace SBD.Models
     public class Lot
     {
         [Key]
+        [Display(Name = "Lot")]
         public int id_lotu { get; set; }
+        [Display(Name = "Data lotu")]
         public DateTime data { get; set; }
         public Hashtable miejsca = new Hashtable();
+        [Display(Name = "Samolot")]
         public int id_samolotu { get; set; }
         public virtual Samolot Samolot { get; set; }
 
+        [Display(Name = "Lotnisko Startowe")]
         public int id_lotniska_startowego { get; set; }
         public virtual Lotnisko Lotnisko { get; set; }
-        
+        [Display(Name = "Lotnisko końcowe")]
         public int id_lotniska_koncowego { get; set; }
-        public virtual Lotnisko Lotnisko_Koncowe { get; set; }        
+        public virtual Lotnisko Lotnisko_Koncowe { get; set; }
 
+        [Display(Name = "Kapitan samolotu")]
         public int id_kapitana {get; set; }
         public virtual Pilot Kapitan { get; set; }
+        [Display(Name = "Oficer samolotu")]
         public int id_oficera { get; set; }
         public virtual Pilot Oficer { get; set; }
         [ForeignKey("id_lotu")]           ////must add to map bilet.id_lotu to this.id_lotu
