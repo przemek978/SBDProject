ab2a90603d65c5baa00e3e2c5361db72fec437f8
przemek978 <przemek.ku2000@gmail.com>
2022-12-08 19:34:57 +0100

    ii

--- Models/User.cs
+++ Models/User.cs
@@ -4,10 +4,10 @@
     {
         public string nazwa_uzytkownika { get; set; }
         public string haslo { get; set; }
-        public User(string username,string password)
-        {
-            this.nazwa_uzytkownika = username;
-            this.haslo = password;
-        }
+        //public User(string username,string password)
+        //{
+        //    this.nazwa_uzytkownika = username;
+        //    this.haslo = password;
+        //}
     }
 }
