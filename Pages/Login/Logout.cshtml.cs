482448962995932b0eee86ee534a3c7ba3f141ba
illelI <ilellixd@gmail.com>
2022-12-10 16:27:43 +0100

    ligin fix

---
+++ Pages/Login/Logout.cshtml.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
+
+namespace SBD.Pages.Login
+{
+    public class LogoutModel : PageModel
+    {
+		public async Task<IActionResult> OnGet()
+		{
+			await HttpContext.SignOutAsync("CookieAuthentication");
+			return this.RedirectToPage("/index");
+		}
+    }
+}
