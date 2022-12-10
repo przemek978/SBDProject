22d688ba772fe239a3a69b422052dffc75731f77
PKuczynski <przemek.ku2000@gmail.com>
2022-12-10 16:28:53 +0100

    gg

---
+++ Pages/Login/Logout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SBD.Pages.Login
+{
+    public class LogoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
